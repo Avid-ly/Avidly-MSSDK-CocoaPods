6c872839be45de96bfc9d8f555315736ea0aad2b
steve <909476821@qq.com>
2021-02-26 15:43:18 +0800

    unity_1003

---
+++ Framework/MSSDK.framework/Headers/MSSDKVersion.h
@@ -0,0 +1,14 @@
+//
+//  MSSDKVERSION.h
+//  MeteorShowSDK
+//
+//  Created by steve on 2020/4/21.
+//  Copyright © 2020 UPLTV Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//sdk版本号
+#define MSSDKVERSION  @"1003"
+#define MSSDKBuild    @"1"
+#define MSSDKVERSIONAndBuild ([NSString stringWithFormat:@"%@.%@",MSSDKVERSION,MSBuild])
