27f297ba8c072efa99c3904b0ebfb87ed1c3a735
steve <909476821@qq.com>
2023-07-14 15:51:14 +0800

    Update 4.0.0.2

---
+++ Framework/MSSDK/MSSDK.xcframework/ios-arm64_x86_64-simulator/MSSDK.framework/Headers/MSSDKVersion.h
@@ -0,0 +1,16 @@
+//
+//  MSSDKVERSION.h
+//  MeteorShowSDK
+//
+//  Created by steve on 2020/4/21.
+//  Copyright © 2020 UPLTV Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//sdk版本号
+#define MSSDKVERSIONSTRING  @"4.0.0.2"
+#define MSSDKVERSION        @"4002"
+#define MSSDKBuild          @"1"
+#define MSSDKVERSIONAndBuild ([NSString stringWithFormat:@"%@.%@",MSSDKVERSION,MSBuild])
+
