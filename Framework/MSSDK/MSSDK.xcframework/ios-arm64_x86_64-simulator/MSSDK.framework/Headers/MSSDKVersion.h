135fb4a2a0b03e9a4feb6ac6eaad71e436b136dc
steve <909476821@qq.com>
2024-06-19 11:35:17 +0800

    Update 4.1.0.3

--- Framework/MSSDK/MSSDK.xcframework/ios-arm64_x86_64-simulator/MSSDK.framework/Headers/MSSDKVersion.h
+++ Framework/MSSDK/MSSDK.xcframework/ios-arm64_x86_64-simulator/MSSDK.framework/Headers/MSSDKVersion.h
@@ -9,8 +9,8 @@
 #import <Foundation/Foundation.h>
 
 //sdk版本号
-#define MSSDKVERSIONSTRING  @"4.1.0.2"
-#define MSSDKVERSION        @"4102"
+#define MSSDKVERSIONSTRING  @"4.1.0.3"
+#define MSSDKVERSION        @"4103"
 #define MSSDKBuild          @"1"
 #define MSSDKVERSIONAndBuild ([NSString stringWithFormat:@"%@.%@",MSSDKVERSION,MSBuild])
 
